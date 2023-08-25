406896fe8397753746c884d38348643ab2c6427a
Bhaskar Gupta <codeforeducations@gmail.com>
2023-08-25 23:13:00 +0530

    React Router V6 Setup & Installation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Component/Home";
+import About from "./Component/About";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello! React Router V6</h1>
+
+      {/* Navigations */}
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/home" element={<Home />} /> */}
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
