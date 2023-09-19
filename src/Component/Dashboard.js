ab34f4a350ee6035792e1471d58397f7c860f779
Bhaskar Gupta <codeforeducations@gmail.com>
2023-09-19 21:32:50 +0530

    uselocation

--- src/Component/Dashboard.js
+++ src/Component/Dashboard.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 function Dashboard() {
+  // Example 1
+  const location = useLocation();
+  console.log(location);
+
   return (
     <>
       <h1>Welcome To Dashboard Page</h1>
 
       {/* Links Page */}
-      <Link to="profile">Profile Page</Link>
+      <Link
+        to="profile"
+        state={{ name: "code4education", owner: "Bhaskar Gupta" }}
+      >
+        Profile Page
+      </Link>
       <br />
       <br />
       <Link to="setting">Setting Page</Link>
